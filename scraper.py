61da32013e218b465ac0e5907433a0d561037902
Paul Taylor <pltorigami@gmail.com>
2023-10-31 15:19:22 -0400

    Add methods to scraper.py to get stream links from sportsurge and bestsolaris

--- scraper.py
+++ scraper.py
@@ -13,7 +13,6 @@ class Scraper:
 
     def get(self, url: str) -> BeautifulSoup:
         page = requests.get(url)
-        # check that server returns < 400 response code
         page.raise_for_status()
 
         soupObject = BeautifulSoup(page.text, self.htmlParser)
@@ -24,7 +23,9 @@ class Scraper:
         with open(fileName) as f:
             return BeautifulSoup(f.read(), self.htmlParser)
 
-    def getGamePages(self):
+    def getGamePages(self) -> dict[str, str]:
+        """Return list of all links of game pages for all games listed on self.url"""
+
         # soup = self.get(self.url)
         soup = self.getFromFile("ugh.html")
         gameDivs = soup.select("main.pushContain div.row div.col-md-4")
@@ -39,7 +40,54 @@ class Scraper:
 
         return gamePageLinks
 
+    def getGameLink(self, url: str) -> str:
+        """Given url to a SportSurge game page, return link to BestSolaris stream
+        if present, or first link listed if not. Return empty string if game is not live
+        """
+
+        # soup = self.get(url)
+        soup = self.getFromFile("soccer.html")
+        eventStatus = soup.select_one("div.event-status")
+        bestLinkIfNoBS = ""
+
+        if eventStatus is not None and eventStatus.text.strip() == "LIVE":
+            linksTableBody = soup.select_one("tbody")
+            if linksTableBody is not None:
+                firstIteration = True
+                for tableRow in linksTableBody.select("tr"):
+                    rowHeader = tableRow.select_one("th")
+                    link = tableRow.select_one("a")
+                    if link is not None:
+                        linkHref = str(link.get("href"))
+                        if linkHref is not None:
+                            if (
+                                rowHeader is not None
+                                and rowHeader.text.strip().lower() == "bestsolaris"
+                            ):
+                                return linkHref
+                            if firstIteration:
+                                bestLinkIfNoBS = linkHref
+                                firstIteration = False
+
+        return bestLinkIfNoBS
+
+    def getStreamLinkFromBSPage(self, url: str) -> str:
+        """Given BestSolaris stream page, return link to steam link for embedding"""
+
+        # soup = self.getFromFile("torino.html")
+        soup = self.get(url)
+        shortlink = soup.select_one("link[rel='shortlink']")
+
+        if shortlink is not None:
+            linkHref = shortlink.get("href")
+            if linkHref is not None:
+                return str(linkHref)
+
+        raise Exception("BestSolaris streaming link not found")
+
 
 if __name__ == "__main__":
     scr = Scraper()
     scr.getGamePages()
+    a = scr.getGameLink("a")
+    print(a)
